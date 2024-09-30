e77b1e40485dcd19a07c6a703606daa315218eb9
Maksym <64355802+Maxtone01@users.noreply.github.com>
2024-09-30 19:38:13 +0300

    Update UIManager.cs

--- Assets/_Scripts/UI/UIManager.cs
+++ Assets/_Scripts/UI/UIManager.cs
@@ -26,8 +26,8 @@ namespace _Scripts.UI
 
         public void ShowHidePauseMenu()
         {
-            _pauseMenu.gameObject.SetActive(!_pauseMenu.activeSelf);
             Cursor.visible = !_pauseMenu.activeSelf;
+            _pauseMenu.gameObject.SetActive(!_pauseMenu.activeSelf);
         } 
 
         private void ShowEndGamePanel()
